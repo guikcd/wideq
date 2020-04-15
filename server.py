1e701d8b8b71299e0c5048fa541c4fc4abc9aa11
pifou <nicolas_gras@hotmail.com>
2020-04-15 11:19:42 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import wideq
+import ssl
 import time
 import argparse
 import sys
 import re
 import os.path
 import logging
+import traceback
 from typing import List
 from flask import Flask, json, jsonify
 
@@ -14,18 +16,20 @@ api = Flask(__name__)
 
 LOGGER = logging.getLogger("wideq.server")
 STATE_FILE = 'wideq_state.json'
-# the global object for wideq client:
-client = None
+
 # state is client global config
 state = {}
 
 
 @api.route('/ping', methods=['GET'])
 def get_ping():
-    return json.dumps({"running": "ok"})
+    """check if server is alive"""
+    return jsonify({"running": "ok"}), 200
 
-@api.route('/log/<level>', methods=['GET', 'POST'])
+@api.route('/log/<string:level>', methods=['GET', 'POST'])
 def set_log(level):
+    """change log level for application: [debug|info|warn|error]"""
+    level = level.lower()
     if level == 'debug':
         lvl = logging.DEBUG
     elif level == 'info':
@@ -39,23 +43,23 @@ def set_log(level):
 
     wideq.set_log_level(lvl)
     LOGGER.setLevel(lvl)        
-    return jsonify(log=level,result="ok"), 200
+    return jsonify({'log':level,'result':'ok'}), 200
 
-@api.route('/save/', methods=['GET'])
+@api.route('/save', methods=['GET'])
 def get_save_default():
     return get_save(STATE_FILE)
 
 @api.route('/save/<string:file>', methods=['GET'])
 def get_save(file):
-    # Save the updated state.
+    """Save the updated state to a local json file"""
     with open(file, 'w') as f:
         json.dump(state, f)
         LOGGER.debug("Wrote state file '%s'", os.path.abspath(file))
-    return jsonify(result='ok'), 200
+    return jsonify({'result':'ok'}), 200
 
-@api.route('/auth/<string:country>/<string:language>', methods=['GET'])
+@api.route('/gateway/<string:country>/<string:language>', methods=['GET'])
 def get_auth(country, language):
-    # get the auth Url for country and market
+    """get the auth Url for country and market"""
     global state
 
     LOGGER.debug('get auth with country %s and lang %s ', country, language)
@@ -65,21 +69,21 @@ def get_auth(country, language):
 
     country_regex = re.compile(r"^[A-Z]{2,3}$")
     if not country_regex.match(country):
-        LOGGER.error("Country must be two or three letters"
-           " all upper case (e.g. US, NO, KR) got: '%s'",
-           country)
-        return "err"
+        msg = "Country must be two or three letters" \
+           " all upper case (e.g. US, NO, KR) got: '{}'".format( country)
+        LOGGER.error(msg)
+        return jsonify({"err":msg}), 401
 
     if not language:
         language = wideq.DEFAULT_LANGUAGE
 
     language_regex = re.compile(r"^[a-z]{2,3}-[A-Z]{2,3}$")
     if not language_regex.match(language):
-        LOGGER.error("Language must be a combination of language"
-           " and country (e.g. en-US, no-NO, kr-KR)"
-           " got: '%s'",
-           language)
-        return "err"
+        msg = "Language must be a combination of language" \
+           " and country (e.g. en-US, no-NO, kr-KR)" \
+           " got: '{}'".format(language)
+        LOGGER.error(msg)
+        return jsonify({"err":msg}), 401
 
     LOGGER.info("auth country=%s, lang=%s", country, language )
 
@@ -94,23 +98,23 @@ def get_auth(country, language):
     # Save the updated state.
     state = client.dump()
         
-    return jsonify(url=login_url), 200
+    return jsonify({'url':login_url}), 200
 
-@api.route('/auth/', methods=['GET'])
+@api.route('/auth', methods=['GET'])
 def get_auth_default():
     LOGGER.debug('get default auth')
     return get_auth(wideq.DEFAULT_COUNTRY, wideq.DEFAULT_LANGUAGE)
 
 @api.route('/token/<path:token>', methods=['GET', 'POST'])
 def get_token(token):
-    # url from LG login with the token
+    """URL from LG login with the token"""
     global state
 
     client = wideq.Client.load(state)
     client._auth = wideq.Auth.from_url(client.gateway, token)
     # Save the updated state.
     state = client.dump()
-    return jsonify(token="ok"), 200
+    return jsonify({'token':'ok'}), 200
 
 
 #
@@ -119,19 +123,28 @@ def get_token(token):
 #
 #
 
-@api.route('/ls/', methods=['GET' ])
+@api.route('/ls', methods=['GET' ])
 def get_ls():
     """List the user's devices."""
+    global state
+    
     client = wideq.Client.load(state)
+    LOGGER.debug('ls with client=' + json.dumps(client.dump()))
+    client.refresh()
+    LOGGER.debug('ls with client=' + json.dumps(client.dump()))
+    
     # Loop to retry if session has expired.
     while True:
         try:
             result = []
             for device in client.devices:
-                print('{0.id}: {0.name} ({0.type.name} {0.model_id})'.format(device))
-                result.append({id:device.id, name:device.name, type:device.type.name, model:device.model_id})
-            print(result)
+                LOGGER.debug('{0.id}: {0.name} ({0.type.name} {0.model_id})'.format(device))
+                result.append({'id':device.id, 'name':device.name, 'type':device.type.name, 'model':device.model_id})
+            LOGGER.debug(str(len(result)) + ' elements: ' + str(result))
 
+            # Save the updated state.
+            state = client.dump()
+            
             return jsonify(result), 200
 
         except wideq.NotLoggedInError:
@@ -140,13 +153,15 @@ def get_ls():
 
         except UserError as exc:
             LOGGER.error(exc.msg)
-            return jsonify(exc.msg), 401
-
-def mon(client, device_id):
+            return jsonify({'msg':exc.msg}), 401
+            
+@api.route('/mon/<device>', methods=['GET' ])
+def mon( device_id):
     """Monitor any device, displaying generic information about its
     status.
     """
 
+    client = wideq.Client.load(state)
     device = client.get_device(device_id)
     model = client.model_info(device)
 
@@ -263,7 +278,35 @@ def ac_config(client, device_id):
     print(ac.get_light())
     print(ac.get_zones())
 
+def _build_ssl_context(maximum_version=None, minimum_version=None):
+    if not hasattr(ssl, "SSLContext"):
+        raise RuntimeError("httplib2 requires Python 3.2+ for ssl.SSLContext")
 
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    context.verify_mode = ssl.CERT_NONE
+
+    # SSLContext.maximum_version and SSLContext.minimum_version are python 3.7+.
+    # source: https://docs.python.org/3/library/ssl.html#ssl.SSLContext.maximum_version
+    if maximum_version is not None:
+        if hasattr(context, "maximum_version"):
+            context.maximum_version = getattr(ssl.TLSVersion, maximum_version)
+        else:
+            raise RuntimeError("setting tls_maximum_version requires Python 3.7 and OpenSSL 1.1 or newer")
+    if minimum_version is not None:
+        if hasattr(context, "minimum_version"):
+            context.minimum_version = getattr(ssl.TLSVersion, minimum_version)
+        else:
+            raise RuntimeError("setting tls_minimum_version requires Python 3.7 and OpenSSL 1.1 or newer")
+
+    # check_hostname requires python 3.4+
+    # we will perform the equivalent in HTTPSConnectionWithTimeout.connect() by calling ssl.match_hostname
+    # if check_hostname is not supported.
+    if hasattr(context, "check_hostname"):
+        context.check_hostname = False
+
+    return context 
 
 if __name__ == '__main__':
-    api.run()
+    logging.basicConfig(filename='server.log',level=logging.INFO)
+    context = _build_ssl_context( 'TLSv1', 'TLSv1')
+    api.run(port=5025)
