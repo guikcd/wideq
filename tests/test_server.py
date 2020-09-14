3cb5565b3823c4347ba7ffe56af9568434d6aa02
pifou <nicolas_gras@hotmail.com>
2020-09-14 21:03:25 +0200

    Update test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,13 +1,4 @@
-import json
 import unittest
-import ssl
-import time
-import argparse
-import re
-import uuid
-import os.path
-import logging
-import requests
 from flask import Flask, json, jsonify, request
 from flask.logging import create_logger
 from werkzeug.exceptions import HTTPException
@@ -50,17 +41,6 @@ class WideqServerTest(unittest.TestCase):
         with open('./tests/fixtures/client.json') as fp:
             state = json.load(fp)
         self.client = Client.load(state)
-        self.device_info = DeviceInfo({
-            'alias': 'WASHER',
-            'deviceId': '33330ba80-107d-11e9-96c8-0051ede85d3f',
-            'deviceType': 201,
-            'modelJsonUrl': (
-                'https://aic.lgthinq.com:46030/api/webContents/modelJSON?'
-                'modelName=F3L2CYV5W_WIFI&countryCode=WW&contentsId='
-                'JS1217232703654216&authKey=thinq'),
-            'modelNm': 'F3L2CYV5W_WIFI',
-        })
-        self.washer = WasherDevice(self.client, self.device_info)
 
         app = server.create_app()
         app.debug = True
@@ -69,11 +49,11 @@ class WideqServerTest(unittest.TestCase):
     ###############
     #### tests ####
     ###############
- 
+
     def test_main_page(self):
         response = self.app.get('/ping', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
- 
- 
+
+
 if __name__ == "__main__":
     unittest.main()
